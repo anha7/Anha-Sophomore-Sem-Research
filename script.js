a25ffd900e5b3699b06c0ff7eb159637d732e16b
Anha Khan <anhak@princeton.edu>
2024-04-15 22:19:10 -0400

    finished visuals

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const title1 = document.getElementById("title1");
 const title2 = document.getElementById("title2");
 const title3 = document.getElementById("title3");
-const title4 = document.getElementById("title4");
 const author = document.getElementById("author");
 const legend = document.getElementById("legend");
 const mapContainer = document.getElementById('mapContainer');
@@ -15,7 +14,6 @@ function openAnim() {
     title1.classList.add("opacity");
     title2.classList.add("opacity");
     title3.classList.add("opacity");
-    title4.classList.add("opacity");
     author.classList.add("opacity");
 }
 
